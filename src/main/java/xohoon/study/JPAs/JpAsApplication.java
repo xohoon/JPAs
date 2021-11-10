68a2c8081cdb8466477c1342cd3aca3d304b879e
xohoon <xohoonx2@gmail.com>
2021-11-10 22:51:19 +0900

    JPA study project

---
+++ src/main/java/xohoon/study/JPAs/JpAsApplication.java
@@ -0,0 +1,14 @@
+package xohoon.study.JPAs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpAsApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/xohoon/study/JPAs/JpAsApplicationTests.java
@@ -0,0 +1,13 @@
+package xohoon.study.JPAs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
