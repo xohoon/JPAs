6c90da42baab84b3146106d67f42c03a02983a93
xohoon <xohoonx2@gmail.com>
2021-11-11 00:59:37 +0900

    JPA DB connection and test setting

--- src/main/java/xohoon/study/JPAs/HelloController.java
+++ src/main/java/xohoon/study/JPAs/HelloController.java
@@ -1,3 +1,5 @@
+package xohoon.study.JPAs;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/main/java/xohoon/study/JPAs/MemberRepository.java
@@ -0,0 +1,24 @@
+package xohoon.study.JPAs;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+//  Spring Boot는 entity manager 기본 세팅 다해줌
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
