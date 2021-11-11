e2eb514df997ecca0256ae08176b477781fa8334
xohoon <xohoonx2@gmail.com>
2021-11-12 03:59:36 +0900

    stock add and remove code -> 상품 비즈니스 로직을 상품 entity에 넣는것이 가장 객체지향적

--- src/main/java/xohoon/study/JPAs/domain/item/Item.java
+++ src/main/java/xohoon/study/JPAs/domain/item/Item.java
@@ -3,6 +3,7 @@ package xohoon.study.JPAs.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import xohoon.study.JPAs.domain.Category;
+import xohoon.study.JPAs.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -25,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+
+    /*
+    * stock 증가
+    * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+    * stock 감소
+    * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
