5e94c511762a1c5564978de250cc29f7766df134
xohoon <xohoonx2@gmail.com>
2021-11-14 01:45:07 +0900

    html code and controller(member, item) ** 머지와 변경감지 **

---
+++ src/main/java/xohoon/study/JPAs/controller/MemberController.java
@@ -0,0 +1,56 @@
+package xohoon.study.JPAs.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import xohoon.study.JPAs.domain.Address;
+import xohoon.study.JPAs.domain.Member;
+import xohoon.study.JPAs.service.MemberService;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> member = memberService.findMembers(); // API 설계 시에는 Entity를 절대 외부 반환하면 안된다.
+        model.addAttribute("members", member);
+
+        return "members/memberList";
+    }
+}
--- src/main/java/xohoon/study/JPAs/service/ItemService.java
+++ src/main/java/xohoon/study/JPAs/service/ItemService.java
@@ -3,6 +3,7 @@ package xohoon.study.JPAs.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import xohoon.study.JPAs.domain.item.Book;
 import xohoon.study.JPAs.domain.item.Item;
 import xohoon.study.JPAs.repository.ItemRepository;
 
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional // 왠만하면 merge 쓰지말고 변경감지 사용하자 -> merge는 값이 없으면 null로 세팅
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+
+        return findItem;
     }
 
     public List<Item> findItems() {
