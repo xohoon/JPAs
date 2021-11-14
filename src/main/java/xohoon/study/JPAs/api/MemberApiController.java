aa5bebce5f11eb0b2bf816287992d8474e2a4794
xohoon <xohoonx2@gmail.com>
2021-11-15 03:14:33 +0900

    수정 API 설계

--- src/main/java/xohoon/study/JPAs/api/MemberApiController.java
+++ src/main/java/xohoon/study/JPAs/api/MemberApiController.java
@@ -1,10 +1,9 @@
 package xohoon.study.JPAs.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xohoon.study.JPAs.domain.Member;
 import xohoon.study.JPAs.service.MemberService;
 
@@ -34,7 +33,7 @@ public class MemberApiController {
         }
     }
 
-    // entity 스펙을 변경하면 컴파일 오류나서 API 스펙에는 영향을 주지 않는다.
+    // entity 스펙을 변경하면 컴파일 오류, API 스펙에는 영향을 주지 않는다.
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
@@ -46,6 +45,27 @@ public class MemberApiController {
 
     @Data // DTO 만들면 어떤 값이 오는지 API스펙에 맞춰져서 유지보수에 좋다
     static class CreateMemberRequest {
+        private String name;
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
         private String name;
     }
 
