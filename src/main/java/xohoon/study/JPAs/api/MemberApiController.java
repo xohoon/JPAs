024b4a5ef0243420d2462ee17397d92282d68e5e
xohoon <xohoonx2@gmail.com>
2021-11-15 03:36:54 +0900

    조회 API 설계 - DTO 활용(entity X)

--- src/main/java/xohoon/study/JPAs/api/MemberApiController.java
+++ src/main/java/xohoon/study/JPAs/api/MemberApiController.java
@@ -8,6 +8,8 @@ import xohoon.study.JPAs.domain.Member;
 import xohoon.study.JPAs.service.MemberService;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController // Controller + ResponseBody
 @RequiredArgsConstructor
@@ -15,6 +17,9 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    /*
+     * 회원등록 API
+     * */
     // entity를 변경하면 API스펙이 완변 바뀌어버린다
     // entity 활용한것 -> 실무에서는 entity를 노출하지말고 파라미터로 받지마라(DTO)
     @PostMapping("/api/v1/members")
@@ -48,6 +53,9 @@ public class MemberApiController {
         private String name;
     }
 
+    /*
+     * 회원 수정 API
+     * */
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(
             @PathVariable("id") Long id,
@@ -62,6 +70,7 @@ public class MemberApiController {
     static class UpdateMemberRequest {
         private String name;
     }
+
     @Data
     @AllArgsConstructor
     static class UpdateMemberResponse {
@@ -69,4 +78,41 @@ public class MemberApiController {
         private String name;
     }
 
+    /*
+     * 회원 조회 API
+     * */
+    // entity 안에 order 정보도 포함되어 있다(@JsonIgnore 로 제외시킬 순 있다 하지만 API마다 요청하는 데이터가 다르기 때문에 NO)
+    // array를 바로 반환하면 다양한 api 스펙을 만들 수 없다(json 문법에 어긋남)
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+//        List<MemberDto> collect = memberService.findMembers().stream()
+//                .map(m -> new MemberDto(m.getName()))
+//                .collect(Collectors.toList());
+//        통합해도된다
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 }
