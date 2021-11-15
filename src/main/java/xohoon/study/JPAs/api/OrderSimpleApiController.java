c3a170df622757b735d20bdb43f80f7c674df7d7
xohoon <xohoonx2@gmail.com>
2021-11-15 23:19:12 +0900

    order 조회 v3 : fetch join 활용

--- src/main/java/xohoon/study/JPAs/api/OrderSimpleApiController.java
+++ src/main/java/xohoon/study/JPAs/api/OrderSimpleApiController.java
@@ -51,6 +51,17 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    // V3 : fetch join 활용해서 query 한번에 로딩
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
