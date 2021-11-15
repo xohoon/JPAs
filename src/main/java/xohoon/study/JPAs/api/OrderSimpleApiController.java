4c80962c4c51477492b540c95fc5319ec6e115cb
xohoon <xohoonx2@gmail.com>
2021-11-15 23:05:53 +0900

    order 조회v2 : DTO 활용 N+1 문제 발생

--- src/main/java/xohoon/study/JPAs/api/OrderSimpleApiController.java
+++ src/main/java/xohoon/study/JPAs/api/OrderSimpleApiController.java
@@ -1,15 +1,18 @@
 package xohoon.study.JPAs.api;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xohoon.study.JPAs.domain.Address;
 import xohoon.study.JPAs.domain.Order;
+import xohoon.study.JPAs.domain.OrderStatus;
 import xohoon.study.JPAs.repository.OrderRepository;
 import xohoon.study.JPAs.repository.OrderSearch;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * xToOne(ManyToOne, OneToOne)
@@ -23,7 +26,7 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-    // member <-> order 무한루프
+    // V1 : entity 직접 활용, member <-> order 무한루프
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -34,4 +37,34 @@ public class OrderSimpleApiController {
         return all;
     }
 
+    // V2 : DTO 활용 -> lazy
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        // ORDER
+        // N + 1 -> 1 + N(2) = 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
+    }
 }
